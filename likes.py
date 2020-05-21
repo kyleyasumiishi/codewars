4e030bfe038c4cd878c1fbc993ad1b4a52c665d5
Kyle <k.yasumiishi@gmail.com>
2020-05-20 22:59:18 -0700

    likes

---
+++ likes.py
@@ -0,0 +1,22 @@
+def likes(names):
+    message = ""
+    num_likes = len(names)
+    if num_likes == 0:
+        message = "no one likes this"
+    elif num_likes == 1:
+        message = str(names[0]) + " likes this"
+    elif num_likes == 2:
+        message = str(names[0]) + " and " + str(names[1]) + " like this"
+    elif num_likes == 3:
+        message = str(names[0]) + ", " + str(names[1]) + " and " + str(names[2]) + " like this"
+    else:
+        message = str(names[0]) + ", " + str(names[1]) + " and " + str(num_likes - 2) + " others like this"
+    return message
+    
+print(likes([]))    
+print(likes(["Kyle"]))
+print(likes(["Kyle", "Erica"]))
+print(likes(["Kyle", "Erica", "Niki"]))
+print(likes(["Kyle", "Erica", "Niki", "Jazz"]))
+print(likes(["Kyle", "Erica", "Niki", "Jazz", "Tony", "Harvey"]))
+print(likes(["Kyle", "Erica", "Niki", "Jazz", "Tony", "Scott", "Harvey"]))
